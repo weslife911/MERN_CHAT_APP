7d95863f868d405b6847f59c727b83a22aa9e064
weslife911 <mfonfubikoma@gmail.com>
2025-08-22 00:31:20 +0100

    added frontend api url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1/" : "/api/v1/"
+    baseURL: import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:8080/api/v1/" : import.meta.env.VITE_API_URL,
 });
