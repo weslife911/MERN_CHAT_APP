7acad2b1a6da08dc0f4752a825ff48f0eaac300e
weslife911 <mfonfubikoma@gmail.com>
2025-08-24 11:36:51 +0100

    changed socket url

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios"
 import toast, {} from "react-hot-toast"
 import { io } from "socket.io-client"
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
